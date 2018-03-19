a38b81e318229ecb44fdcd262f154c69064de5b8
stuhin <stuhin@live.ru>
2018-03-19 22:27:05 +0700

    Added project

---
+++ Wallpapers/MainWindow.xaml.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace Wallpapers
+{
+    public class FileData
+    {
+        public BitmapImage Image { get; set; }
+        public string Path { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(1, 0, 0);
+            dispatcherTimer.Start();
+
+            InitializeComponent();
+
+            System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.Picture;
+            notifyIcon.Visible = true;
+            notifyIcon.Click += delegate (object sender, EventArgs args) 
+            {
+                if (WindowState == WindowState.Normal)
+                {
+                    Hide();
+                    WindowState = WindowState.Minimized;
+                }
+                if (WindowState == WindowState.Minimized)
+                {
+                    Show();
+                    WindowState = WindowState.Normal;
+                }
+            };
+
+            Run();
+        }
+
+        private void SetDataGrid()
+        {
+            string currentpath = Directory.GetCurrentDirectory();
+            Config config = Config.Get();
+            List<FileData> fileDatas = new List<FileData>();
+            foreach (string path in Directory.GetFiles($"{currentpath}\\{config.desktop}"))
+            {
+                FileInfo file = new FileInfo(path);
+                fileDatas.Add(new FileData() { Image = toBitmap(File.ReadAllBytes(path)), Path = path, Created = file.CreationTime });
+            }
+
+            if (fileDatas.Count > 0)
+            {
+                Wallpaper.Set(fileDatas.OrderByDescending(f => f.Created).First().Path);
+            }
+
+            dataGrid.ItemsSource = fileDatas.OrderByDescending(f => f.Created).Take(10);
+        }
+
+        public static BitmapImage toBitmap(Byte[] value)
+        {
+            try
+            {
+                if (value != null && value is byte[])
+                {
+                    byte[] ByteArray = value as byte[];
+                    BitmapImage bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.StreamSource = new MemoryStream(ByteArray);
+                    bmp.EndInit();
+                    return bmp;
+                }
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void SetDesktopBackground_Click(object sender, RoutedEventArgs e)
+        {
+            if (((FrameworkElement)e.Source).DataContext is FileData)
+            {
+                string path = ((FileData)((FrameworkElement)e.Source).DataContext).Path;
+                Wallpaper.Set(path);
+            }
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+            }
+
+            base.OnStateChanged(e);
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Run();
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        private async void Run()
+        {
+            await RunAsync();
+            SetDataGrid();
+        }
+
+        private async Task RunAsync()
+        {
+            string currentpath = Directory.GetCurrentDirectory();
+            Config config = Config.Get();
+
+            WebClient client = new WebClient();
+
+            List<Image> images = new List<Image>();
+            foreach (string culture in config.cultures)
+            {
+                if (config.new_wallpapers)
+                {
+                    List<Image> images2 = GetBingWallpapers(client, culture, -1);
+                    images.AddRange(images2);
+                }
+                if (config.arhive_wallpapers)
+                {
+                    List<Image> images2 = GetBingWallpapers(client, culture, 7);
+                    images.AddRange(images2);
+                }
+            }
+
+            foreach (string resolution in config.resolutions)
+            {
+                if (!Directory.Exists($"{currentpath}\\{resolution}"))
+                {
+                    Directory.CreateDirectory($"{currentpath}\\{resolution}");
+                }
+                List<string> files = Directory.GetFiles($"{currentpath}\\{resolution}").Select(s => s.Replace($"{currentpath}\\{resolution}\\", "").Split('_')[0]).ToList();
+
+                SetLockScreenWallpapers(currentpath, resolution, files);
+                SetBingWallpapers(client, images, files, currentpath, resolution);
+            }
+
+            if (!config.resolutions.Contains(config.desktop))
+            {
+                int width = int.Parse(config.desktop.Split('x')[0]);
+                int height = int.Parse(config.desktop.Split('x')[1]);
+                string resolution = config.resolutions.Where(r => int.Parse(r.Split('x')[0]) > width && int.Parse(r.Split('x')[1]) > height).OrderBy(r => r).FirstOrDefault();
+                if(!string.IsNullOrEmpty(resolution))
+                {
+                    if (!Directory.Exists($"{currentpath}\\{config.desktop}"))
+                    {
+                        Directory.CreateDirectory($"{currentpath}\\{config.desktop}");
+                    }
+
+                    List<string> files = Directory.GetFiles($"{currentpath}\\{resolution}").ToList();
+                    foreach (string file in files)
+                    {
+                        string path = file.Replace(resolution, config.desktop);
+                        using (System.Drawing.Image image = System.Drawing.Image.FromFile(file))
+                        using (System.Drawing.Image newImage = ScaleImage(image, width, height))
+                        {
+                            int x = newImage.Width == width ? 0 : (int)((newImage.Width - width) / 2);
+                            int y = newImage.Height == height ? 0 : (int)((newImage.Height - height) / 2);
+                            Crop(newImage, new Rectangle(x, y, width, height)).Save(file.Replace(resolution, config.desktop), ImageFormat.Jpeg);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SetLockScreenWallpapers(string currentpath, string resolution, List<string> files)
+        {
+            List<string> filesScreen = new List<string>();
+            try
+            {
+                string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                filesScreen = Directory.GetFiles($"{userPath}\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets").ToList();
+            }
+            catch
+            {
+
+            }
+
+            foreach (string file in filesScreen)
+            {
+                try
+                {
+                    System.Drawing.Image imgInput = System.Drawing.Image.FromFile(file);
+                    if (resolution == $"{imgInput.Width}x{imgInput.Height}")
+                    {
+                        string[] nameArray = file.Split('\\');
+                        string name = nameArray[nameArray.Length - 1] + ".jpg";
+                        if (!files.Contains(name))
+                        {
+                            string path = $"{currentpath}\\{resolution}\\{name}";
+                            File.Copy(file, path);
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        private List<Image> GetBingWallpapers(WebClient client, string culture, int idx)
+        {
+            try
+            {
+                string json = client.DownloadString($"https://www.bing.com/HPImageArchive.aspx?format=js&idx={idx}&n=8&mkt={culture}");
+                ImageJson imageJson = new JavaScriptSerializer().Deserialize<ImageJson>(json);
+                return imageJson.images;
+            }
+            catch
+            {
+                return new List<Image>();
+            }
+        }
+
+        private void SetBingWallpapers(WebClient client, List<Image> images, List<string> files, string currentpath, string resolution)
+        {
+            try
+            {
+                foreach (string urlbase in images.Select(i => i.urlbase).Distinct())
+                {
+                    string[] urlbaseArray = urlbase.Split('/');
+                    string namebase = urlbaseArray[urlbaseArray.Length - 1];
+                    string url = $"https://www.bing.com{urlbase}_{resolution}.jpg";
+                    string path = $"{currentpath}\\{resolution}\\{namebase}_{resolution}.jpg";
+                    string name = namebase.Split('_')[0];
+                    if (!files.Contains(name))
+                    {
+                        client.DownloadFile(url, path);
+                        files.Add(name);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        public static System.Drawing.Image Crop(System.Drawing.Image image, Rectangle selection)
+        {
+            Bitmap bmp = image as Bitmap;
+            Bitmap cropBmp = bmp.Clone(selection, bmp.PixelFormat);
+            image.Dispose();
+            return cropBmp;
+        }
+
+        public static System.Drawing.Image ScaleImage(System.Drawing.Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            double ratio = Math.Max(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
+    }
+}
---
+++ Wallpapers/Wallpaper.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Web.Script.Serialization;
+
+namespace Wallpapers
+{
+    public class Config
+    {
+        public bool new_wallpapers { get; set; }
+        public bool arhive_wallpapers { get; set; }
+        public bool lockscreen_to_wallpapers { get; set; }
+        public string desktop { get; set; }
+        public string[] resolutions { get; set; }
+        public string[] cultures { get; set; }
+
+        public static Config Get()
+        {
+            if (!File.Exists("config.json"))
+            {
+                Config config = new Config()
+                {
+                    new_wallpapers = true,
+                    arhive_wallpapers = true,
+                    lockscreen_to_wallpapers = true,
+                    desktop = "1920x1080",
+                    resolutions = new string[] { "1920x1080", "1080x1920"},
+                    cultures = new string[] { "en-US", "fr-FR", "de-DE", "ru-RU", "es-AR", "en-AU", "de-AT", "nl-BE", "fr-BE", "pt-BR", "en-CA", "fr-CA", "zh-HK", "en-IN", "en-ID", "it-IT", "ja-JP", "ko-KR", "en-MY", "es-MX", "nl-NL", "nb-NO", "zh-CN", "pl-PL", "ar-SA", "en-ZA", "es-ES", "sv-SE", "fr-CH", "de-CH", "zh-TW", "tr-TR", "en-GB", "es-US" }
+                };
+                string json = new JavaScriptSerializer().Serialize(config);
+                File.WriteAllText("config.json", json.Replace(",",",\r\n"));
+            }
+            return new JavaScriptSerializer().Deserialize<Config>(File.ReadAllText("config.json"));
+        }
+    }
+
+    public class ImageJson
+    {
+        public List<Image> images { get; set; }
+    }
+
+    public class Image
+    {
+        public string startdate { get; set; }
+        public string fullstartdate { get; set; }
+        public string enddate { get; set; }
+        public string url { get; set; }
+        public string urlbase { get; set; }
+        public string copyright { get; set; }
+    }
+
+    public sealed class Wallpaper
+    {
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public static void Set(string path)
+        {
+            Stream s = File.OpenRead(path);
+
+            System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+
+            key.SetValue(@"WallpaperStyle", "2");
+            key.SetValue(@"TileWallpaper", "0");
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
